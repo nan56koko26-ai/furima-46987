fbf7a499ddf5af9f39e7174fe5cf95b892c049bc
nan56koko26-ai <nan56koko26@gmail.com>
2026-05-15 16:30:17 +0900

    商品一覧表示機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,8 +2,9 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
-    
+    @items = Item.order(created_at: :desc)
   end
+
 
   def new
     @item = Item.new
@@ -15,6 +16,7 @@ class ItemsController < ApplicationController
     if @item.save
       redirect_to root_path
     else
+      @item = Item.includes(:user)
       render :new, status: :unprocessable_entity
     end
   end
