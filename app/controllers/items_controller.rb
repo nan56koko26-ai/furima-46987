01dc90edca0cb99919de56bf0607971ab3a21e64
nan56koko26-ai <nan56koko26@gmail.com>
2026-05-18 21:15:48 +0900

    商品情報編集機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update]
+  before_action :ensure_owner, only: [:edit, :update]
 
   def index
     @items = Item.includes(:user).order(created_at: :desc)
@@ -20,14 +22,38 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id]) 
+    
   end
   
+  def edit
+    
+  end
+
+  def update
+    if @item.update(item_params_for_update)
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
 
   private
+
   def item_params
     params.require(:item).permit(:name, :image, :description, :price, :category_id, :status_id, :shipping_fee_id, :prefecture_id, :delivery_time_id).merge(user_id: current_user.id)
   end
 
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def ensure_owner
+    if @item.user != current_user then redirect_to root_path
+    end
+  end
+
+  def item_params_for_update
+    params.require(:item).permit(:name, :image, :description, :price, :category_id, :status_id, :shipping_fee_id, :prefecture_id, :delivery_time_id)
+  end
 
 end
